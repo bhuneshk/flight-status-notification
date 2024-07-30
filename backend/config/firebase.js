4569a2dad9226d199ea7c91312cced08b0950e4a
bhunesh9 <bhunesh.chaudhary@marsdevs.com>
2024-07-31 00:29:04 +0530

    .env updated

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -9,14 +9,14 @@ const getMessaging = require('firebase/messaging');
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxO1lIKfUFvFIOjHpGOBMZ5XrJJB-S4AQ",
-  authDomain: "flight-status-update-28fd4.firebaseapp.com",
-  projectId: "flight-status-update-28fd4",
-  storageBucket: "flight-status-update-28fd4.appspot.com",
-  messagingSenderId: "1028053663017",
-  appId: "1:1028053663017:web:79951e7fa79d787ee6cb38",
-  measurementId: "G-YYBESPZKBJ"
-};
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- frontend/src/components/firebase-config.js
+++ frontend/src/components/firebase-config.js
@@ -2,20 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import {getMessaging} from "firebase/messaging";
+require('dotenv').config();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDxO1lIKfUFvFIOjHpGOBMZ5XrJJB-S4AQ",
-  authDomain: "flight-status-update-28fd4.firebaseapp.com",
-  projectId: "flight-status-update-28fd4",
-  storageBucket: "flight-status-update-28fd4.appspot.com",
-  messagingSenderId: "1028053663017",
-  appId: "1:1028053663017:web:79951e7fa79d787ee6cb38",
-  measurementId: "G-YYBESPZKBJ"
-};
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
